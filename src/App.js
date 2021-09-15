de00def708c141f2af4abddabb5cb5daad4f4b61
Romain Caillat <caillat.romain@epitech.eu>
2021-09-15 16:43:35 +0200

    [ADD]: channel page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,7 +9,7 @@ import fondlogin from './img/loginfond.jpg'
 import { useDispatch, useSelector } from 'react-redux'
 import { adduser } from './Userslice'
 
-function Login() {
+function Login({user, setUsers}) {
   var username = "pas def"
   const dispatch = useDispatch()
     return (
@@ -19,7 +19,7 @@ function Login() {
               LOGIN
             </h2>
             <input type="text" name="username" placeholder="Enter Username" onChange={(evt) => { username = evt.target.value }} style={{position: 'absolute', top: window.innerHeight / 4 + 50, left: (window.innerWidth / 2) - 70}} />
-            <button type="button" style={{position: 'absolute', top: window.innerHeight / 4 + 110, left: (window.innerWidth / 2) - 20}} onClick={() => dispatch(adduser({username}))}>
+            <button type="button" style={{position: 'absolute', top: window.innerHeight / 4 + 110, left: (window.innerWidth / 2) - 20}} onClick={() => (dispatch(adduser({username})), setUsers(username))}> 
               <Link to={`/`} activeClassName="current">
                 login
               </Link>
@@ -28,37 +28,57 @@ function Login() {
     )
 }
 
-function Channel() {
-  const listofuser = useSelector(state => state.listuser.listuser);
+function showuser({users}) {
   return(
-  <div>
-    {console.log(listofuser)}
-    <Link to={`/login`} activeClassName="current">
-      go to login
-    </Link>
-    <li>{listofuser}</li>
-  </div>
+    <li >{users}</li>
   )
 }
 
-function SwitchRouter() {
+function Channel({user}) {
+  const listofuser = useSelector(state => state.listuser.listuser);
+  if (user === '') {
+    return(
+    <div>
+      <Link to={`/login`} activeClassName="current">
+        go to login
+      </Link>
+    </div>
+    )
+  } else {
+    return(
+      <div>
+        {console.log(user)}
+        <tr key={listofuser}/>
+        <ul id="etc">
+          {listofuser.map (users => {
+            return showuser({users})
+          })}
+        </ul>
+        <h4>actual user:   {user}</h4>
+      </div>
+      )
+  }
+}
+
+function SwitchRouter({user, setUsers}) {
   return (
     <Switch>
       <Route path="/login">
-        <Login />
+        <Login user={user} setUsers={setUsers}/>
       </Route>
       <Route path="/">
-        <Channel />
+        <Channel user={user}/>
       </Route>
     </Switch>
   )
 }
 
 export default function App() {
+  const [user, setUsers] = useState('');
     return (
         <Router>
           <div style={{margin: 0, padding: 0}}>
-            <SwitchRouter/>
+            <SwitchRouter user={user} setUsers={setUsers}/>
           </div>
         </Router>
     )
